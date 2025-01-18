40e492e0dea9136518d5c3c1140488977bd4ac79
teaglebuilt <dillan.teagle.va@gmail.com>
2025-01-18 16:46:01 -0500

    comment out image

--- tf_modules/talos_cluster/image.tf
+++ tf_modules/talos_cluster/image.tf
@@ -1,23 +1,23 @@
-resource "proxmox_virtual_environment_download_file" "talos_controlplane_image_download" {
-  for_each = toset( formatlist("%s-controlplane", local.proxmox_nodes))
+# resource "proxmox_virtual_environment_download_file" "talos_controlplane_image_download" {
+#   for_each = toset( formatlist("%s-controlplane", local.proxmox_nodes))
 
-  provider     = proxmox
-  node_name    = split("-", each.key)[0]
-  content_type = "iso"
-  datastore_id = "local"
-  file_name    = "talos-controlplane-v${var.talos_data.talos_version}-amd64.iso"
+#   provider     = proxmox
+#   node_name    = split("-", each.key)[0]
+#   content_type = "iso"
+#   datastore_id = "local"
+#   file_name    = "talos-controlplane-v${var.talos_data.talos_version}-amd64.iso"
 
-  url = "https://factory.talos.dev/image/${local.talos_controlplane_schematic_id}/v${var.talos_data.talos_version}/metal-amd64.iso"
-}
+#   url = "https://factory.talos.dev/image/${local.talos_controlplane_schematic_id}/v${var.talos_data.talos_version}/metal-amd64.iso"
+# }
 
-resource "proxmox_virtual_environment_download_file" "talos_worker_image_download" {
-  for_each = toset( formatlist("%s-worker", local.proxmox_nodes))
+# resource "proxmox_virtual_environment_download_file" "talos_worker_image_download" {
+#   for_each = toset( formatlist("%s-worker", local.proxmox_nodes))
 
-  provider     = proxmox
-  node_name    = split("-", each.key)[0]
-  content_type = "iso"
-  datastore_id = "local"
-  file_name    = "talos-worker-v${var.talos_data.talos_version}-amd64.iso"
+#   provider     = proxmox
+#   node_name    = split("-", each.key)[0]
+#   content_type = "iso"
+#   datastore_id = "local"
+#   file_name    = "talos-worker-v${var.talos_data.talos_version}-amd64.iso"
 
-  url = "https://factory.talos.dev/image/${local.talos_worker_schematic_id}/v${var.talos_data.talos_version}/metal-amd64.iso"
-}
+#   url = "https://factory.talos.dev/image/${local.talos_worker_schematic_id}/v${var.talos_data.talos_version}/metal-amd64.iso"
+# }
