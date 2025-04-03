4d86e82df06916bd56629524a24fb11a24a79d54
teaglebuilt <dillan.teagle.va@gmail.com>
2025-04-03 08:59:50 -0400

    setup envoy gateway and disabled gateway in cilium

--- scripts/check-gpu-passthrough.sh
+++ scripts/check-gpu-passthrough.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# check pcie device for nvidia gpu is assigned to the right iommu group
-for group in /sys/kernel/iommu_groups/*; do
-  echo "IOMMU Group $(basename "$group"):"
-  for device in "$group"/devices/*; do
-    lspci -nns "$(basename "$device")"
+check_iommu_groups() {
+  # check pcie device for nvidia gpu is assigned to the right iommu group
+  for group in /sys/kernel/iommu_groups/*; do
+    echo "IOMMU Group $(basename "$group"):"
+    for device in "$group"/devices/*; do
+      lspci -nns "$(basename "$device")"
+    done
+    echo ""
   done
-  echo ""
-done
+}
+
+check_device_state() {
+  lspci -s 2e:00.0 -vv
+}
