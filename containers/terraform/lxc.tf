793773fd52c020c7e95d7078b0a8b5606098fcae
teaglebuilt <dillan.teagle.va@gmail.com>
2025-04-08 14:36:57 -0400

    fix portainer

--- containers/terraform/lxc.tf
+++ containers/terraform/lxc.tf
@@ -67,17 +67,11 @@ resource "proxmox_virtual_environment_container" "portainer" {
     }
 
     inline = [
-      "sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl software-properties-common git direnv",
-      # Add Docker’s official GPG key
-      "sudo mkdir -p /etc/apt/keyrings",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null",
-      # Set up the repository
-      "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu focal stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "sudo apt update",
-      "sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin",
-      # Ensure Docker starts
-      "sudo systemctl enable --now docker",
-      "sudo usermod -aG docker root"
+      "apt update && sudo apt install -y apt-transport-https ca-certificates curl software-properties-common git direnv",
+      "mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | tee /etc/apt/keyrings/docker.asc > /dev/null",
+      "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu focal stable' | tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+      "systemctl enable --now docker && usermod -aG docker root"
     ]
   }
 
@@ -90,13 +84,8 @@ resource "proxmox_virtual_environment_container" "portainer" {
     }
 
     inline = [
-      <<-EOF
-      docker run -d --name portainer_agent -p 9001:9001 \
-          --restart=always \
-          --volume /var/run/docker.sock:/var/run/docker.sock \
-          --volume /var/lib/docker/volumes:/mnt/preview/docker/volumes \
-          portainer/agent:2.19.4
-      EOF
+      "git clone https://github.com/teaglebuilt/homelab && cd homelab",
+      "docker compose -f containers/compose.yaml up -d"
     ]
   }
 }
