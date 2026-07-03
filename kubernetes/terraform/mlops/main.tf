820b3b45696ff23ab9876c09f56656f14b02fe59
teaglebuilt <dillan.teagle.va@gmail.com>
2026-07-03 14:57:28 -0400

    cleanup folder

---
+++ kubernetes/terraform/mlops/main.tf
@@ -0,0 +1,64 @@
+module "talos_cluster" {
+  # source = "git::https://github.com/teaglebuilt/homelab.git//tf_modules/talos_cluster?ref=main"
+  source = "../../../tf_modules/talos_cluster"
+
+  proxmox_ssh_private_key = var.proxmox_ssh_private_key
+
+  image = {
+    version = "v1.11.5"
+    update_version = "v1.11.5" # renovate: github-releases=siderolabs/talos
+  }
+
+  cluster = {
+    name                = "mlops"
+    endpoint            = var.k8s_api_server_ip
+    gateway             = var.network_gateway
+    talos_version       = "v1.11.5"
+    kubernetes_version  = "1.32.2"
+    proxmox_cluster     = "mlops"
+    logging_server      = var.graylog_ip
+    # Explicit (matches the prior Talos default). administration uses 10.245.0.0/16
+    # so pod CIDRs stay non-overlapping for ClusterMesh.
+    pod_subnet          = "10.244.0.0/16"
+    service_subnet      = "10.96.0.0/12"
+  }
+
+  nodes = {
+    "mlops-ctrl-00" = {
+      host_node     = "pve2"
+      machine_type  = "controlplane"
+      ip            = var.master_node_ip
+      vm_id         = 100
+      cpu           = 8
+      disk_size     = 20
+      ram_dedicated = 8096
+    }
+    "mlops-work-00" = {
+      host_node     = "pve2"
+      machine_type  = "worker"
+      ip            = var.worker_one_node_ip
+      vm_id         = 102
+      cpu           = 16
+      disk_size     = 200
+      ram_dedicated = 16384
+      igpu          = true
+      pci           = {
+        id = "10de:2783"
+        name = "nvidia_4070_super"
+        iommu_group = 20
+        node = "pve2"
+        path = "0000:2e:00.0"
+        subsystem_id = "10de:18fe"
+      }
+    }
+    "mlops-work-01" = {
+      host_node     = "pve2"
+      machine_type  = "worker"
+      ip            = var.worker_two_node_ip
+      vm_id         = 103
+      cpu           = 6
+      disk_size     = 40
+      ram_dedicated = 8120
+    }
+  }
+}
---
+++ kubernetes/terraform/mlops/variables.tf
@@ -0,0 +1,33 @@
+variable "k8s_api_server_ip" {
+  description = "ip address of k8s api server"
+  type = string
+}
+
+variable "master_node_ip" {
+  description = "ip address of master node"
+  type = string
+}
+
+variable "worker_one_node_ip" {
+  description = "ip address of worker node"
+  type = string
+}
+
+variable "worker_two_node_ip" {
+  description = "ip address of worker node"
+  type = string
+}
+
+variable "network_gateway" {
+  description = "ip address of proxmox network gateway"
+  type = string
+}
+
+variable proxmox_ssh_private_key {
+  description = "Path to the SSH private key file"
+  type        = string
+}
+
+variable "graylog_ip" {
+  type = string
+}
